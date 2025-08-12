12c0b944cf8bc7dbf73a135dc0c431ea5dd668c3
Karantechie <karankpguinfo001018@gmail.com>
2025-08-12 22:31:38 +0530

    Implement user profile update functionality and refine user retrieval logic

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,9 +1,13 @@
+
+const userModel = require("../models/users.models");
 //GET USER INFO
+// const userModel = require("../models/users.models");
 
 const getUserController = async(req,res) => {
     try {
         //find User
-        const user = await usersModels.findById({_id:req.body.id});
+        // const user = await userModel.findById(req.user.id);
+        const user = await userModel.findById(req.user.id, {_id: 0}, {password: 0}); // Exclude _id and password from the response
         //validation
         if(!user) {
             return res.status(404).send({
@@ -11,6 +15,13 @@ const getUserController = async(req,res) => {
                 message: "User Not Found"
             });
         }
+        //send user data (excluding password)
+        const { password, ...userData } = user._doc;
+        return res.status(200).send({
+            success: true,
+            message: "User fetched successfully",
+            user: userData
+        });
     } catch (error) {
         console.log(error);
         return res.status(500).send({
@@ -21,4 +32,50 @@ const getUserController = async(req,res) => {
     }
 };
 
-module.exports = {getUserController};
+
+// Update User Profile
+const updateUserController = async (req,res) => {
+    try {
+        //find user
+        const user = await userModel.findById(req.user.id);
+        //validation
+        if(!user) {
+            return res.status(404).send({
+                success: false,
+                message: "User Not Found"
+            });
+        }
+        //update user
+        const { userName, phone, address } = req.body;
+        if (userName) user.userName = userName;
+        if (phone) user.phone = phone;
+        if (address) user.address = address;
+        await user.save();
+        //send response
+        res.status(200).send({
+            success: true,
+            message: "User Updated Successfully",
+            user: {
+                _id: user._id,
+                userName: user.userName,
+                email: user.email,
+                phone: user.phone,
+                address: user.address,
+                userType: user.userType,
+                profile: user.profile,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt
+            }
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false,
+            message: "Error in Update User API",
+            error: error.message
+        });
+    }
+}
+
+module.exports = {getUserController, updateUserController};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { getUserController } = require('../controllers/user.controllers');
-const { authMiddleware } = require('../middlewares/authMiddleware');
-const authMiddlewares = require('../middlewares/authMiddleware');
+const { getUserController, updateUserController } = require('../controllers/user.controllers');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-//routes
 // GET USER || GET
-router.get('/getUser', authMiddlewares, getUserController);
+router.get('/getUser', authMiddleware, getUserController);
+
+// UPDATE Profile
+router.put('/updateUser', authMiddleware, updateUserController);
 
 module.exports = router;
